93a0fd78bb2ffc6f1ae1c5dd6c964db27b65586a
Zoé Goey <zoe.goey@gmail.com>
2021-02-15 17:56:53 +0100

    Finally a full working version, but training.py still needs some work, because it loads the data from an external URL instead of loading it from within AzureML.

--- starter_file/train.py
+++ starter_file/train.py
@@ -9,69 +9,75 @@ from azureml.core.run import Run
 from azureml.core import Dataset, Workspace
 import numpy as np
 import pandas
+from skimage import color
 import joblib
 import os
-from transformer import LabTransformer
-       
-    
-def main():
-    # Add arguments to script
-    parser = argparse.ArgumentParser()
 
-    parser.add_argument('--n_neighbors', type=int, default=10, 
-    help="Number of neighbors to consider")
-    parser.add_argument('--weights',  type=int, choices=range(2), 
-    default=0, help="Sample weighting method")
-    parser.add_argument('--embedding', type=int, choices=range(3), 
-    default=0, help="Type of feature embedding")
-    
-    args = parser.parse_args()
-    
-    weights_dict = {0:'uniform', 1:'distance'}
-    embedding_dict = {0:'none', 1:'lab', 2:'nca'}
-    
-    # Fetch the data
-    #df = pandas.read_csv(
-    #"https://raw.githubusercontent.com/zgoey/azure_ml_capstone/master/color_shades.csv")
-    ws = Workspace.from_config()
-    df = Dataset.get_by_name(workspace, 'color_shades').to_pandas_dataframe()
+# +
+# Add arguments to script
+parser = argparse.ArgumentParser()
 
-    # Separate features and target
-    x = df[['Red','Green','Blue']].to_numpy()
-    le = LabelEncoder()
-    y = le.fit_transform(df['Shade'])
+parser.add_argument('--n_neighbors', type=int, default=10, 
+help="Number of neighbors to consider")
+parser.add_argument('--weights',  type=int, choices=range(2), 
+default=0, help="Sample weighting method")
+parser.add_argument('--embedding', type=int, choices=range(3), 
+default=0, help="Type of feature embedding")
 
-    # Setup the run
-    run = Run.get_context()
-    run.log("Number of neighbors:", np.int(args.n_neighbors))
-    run.log("Sample weights:", weights_dict[args.weights])
-    run.log("Feature embedding:", embedding_dict[args.embedding])
-    
-    neigh = KNeighborsClassifier(n_neighbors=args.n_neighbors, 
-    weights=weights_dict[args.weights])
-    
-    # Setup the model
-    if embedding_dict[args.embedding] == 'lab':
-        lab = LabTransformer()
-        model = Pipeline([('embedder', lab), ('knn', neigh)])
-    elif embedding_dict[args.embedding] == 'nca':
-        nca = NeighborhoodComponentsAnalysis()
-        model = Pipeline([('embedder', nca), ('knn', neigh)])
-    else:
-        model = neigh
+args = parser.parse_args()
 
-    # Log the accuracy
-    cv_results = cross_validate(model, x, y, cv=5)
-    accuracy = np.mean(cv_results['test_score'])
-    run.log("Accuracy", np.float(accuracy))
-    
-    # Train final model
-    neigh.fit(x,y)
-    
-    # Save the model
-    os.makedirs('outputs', exist_ok=True)
-    joblib.dump(model, 'outputs/model.pkl')
+class LabTransformer():
+    # here you define the operation it should perform
+    def transform(self, X, y=None, **fit_params):
+        return color.rgb2lab(X.astype(np.uint8)).astype(np.float32)
+
+    # just return self
+    def fit(self, X, y=None, **fit_params):
+        return self
 
 
-if __name__ == '__main__':
-    main()
+weights_dict = {0:'uniform', 1:'distance'}
+embedding_dict = {0:'none', 1:'lab', 2:'nca'}
+
+# Fetch the data
+df = pandas.read_csv(
+"https://raw.githubusercontent.com/zgoey/azure_ml_capstone/master/color_shades.csv")
+#ws = Workspace.from_config()
+#df = Dataset.get_by_name(workspace, 'color_shades').to_pandas_dataframe()
+
+# Separate features and target
+x = df[['Red','Green','Blue']].to_numpy()
+le = LabelEncoder()
+y = le.fit_transform(df['Shade'])
+
+# Setup the run
+run = Run.get_context()
+run.log("Number of neighbors:", np.int(args.n_neighbors))
+run.log("Sample weights:", weights_dict[args.weights])
+run.log("Feature embedding:", embedding_dict[args.embedding])
+
+neigh = KNeighborsClassifier(n_neighbors=args.n_neighbors, 
+weights=weights_dict[args.weights])
+
+# Setup the model
+if embedding_dict[args.embedding] == 'lab':
+    lab = LabTransformer()
+    model = Pipeline([('embedder', lab), ('knn', neigh)])
+elif embedding_dict[args.embedding] == 'nca':
+    nca = NeighborhoodComponentsAnalysis()
+    model = Pipeline([('embedder', nca), ('knn', neigh)])
+else:
+    model = neigh
+
+# Log the accuracy
+cv_results = cross_validate(model, x, y, cv=5)
+accuracy = np.mean(cv_results['test_score'])
+run.log("Accuracy", np.float(accuracy))
+
+# Fit model
+neigh.fit(x,y)
+
+# Save the model
+os.makedirs('outputs', exist_ok=True)
+joblib.dump(model, 'outputs/model.pkl')
+
